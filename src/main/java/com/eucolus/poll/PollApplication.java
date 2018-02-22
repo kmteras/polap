37943e04cac58f066c8b1af2562d1863d9d6f03c
andreasvija <andreasvija@hotmail.com>
2018-02-22 15:31:12 +0200

    fixed indents in PollApplication.java

--- src/main/java/com/eucolus/poll/PollApplication.java
+++ src/main/java/com/eucolus/poll/PollApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PollApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PollApplication.class, args);
+    }
 }
