39b67e2c5b7ecfc45c7627fac8f01056d6e6e681
Karl Martin Teras <karlnitram@gmail.com>
2018-02-24 20:31:01 +0200

    Added jquery for no redirect post

---
+++ src/main/resources/static/questions.js
@@ -0,0 +1,16 @@
+$("#addQuestion").submit(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+        type: "post",
+        url: $(this).attr("action"),
+        data: $('#addQuestion').serialize(),
+        contentType: "application/x-www-form-urlencoded",
+        success: function(responseData, textStatus, jqXHR) {
+            window.location.reload(false);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+});
