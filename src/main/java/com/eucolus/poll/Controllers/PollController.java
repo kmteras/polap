96551e8739627f4d9d05dc0e13e009a910e467a7
Karl Martin Teras <karlnitram@gmail.com>
2018-02-27 13:07:41 +0200

    Polls have their own pages

--- src/main/java/com/eucolus/poll/Controllers/PollController.java
+++ src/main/java/com/eucolus/poll/Controllers/PollController.java
@@ -3,6 +3,7 @@ package com.eucolus.poll.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,12 @@ public class PollController {
     @GetMapping(value = "")
     public String polls(Model model) {
         return "polls";
+    }
+
+    @GetMapping(value = "/{pollId}")
+    public String poll(@PathVariable(value="pollId") Integer pollId, Model model) {
+        model.addAttribute("pollId", pollId);
+        System.out.println("Test");
+        return "poll";
     }
 }
--- src/main/resources/static/js/polls.js
+++ src/main/resources/static/js/polls.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-    console.log("test");
-    $("#multible_choice").click(function() {
+    var lastQuestion = 0;
+
+    $(".multible_choice").click(function() {
         this.checked = $(this).is(":checked");
     });
+
+    for(var i = 1; i < 4; i++) {
+        $("#answers0").after(
+            '<div class="row" id="answers'+i+'">\n' +
+            '        <div class="input-field col 2">\n' +
+            '            <input type="checkbox" class="filled-in multiple_choice" id="right_answer\'+i+\'"/>\n' +
+            '            <label for="right_answer\'+i+\'">Right Answer</label>\n' +
+            '        </div>\n' +
+            '        <div class="input-field col 3">\n' +
+            '            <input id="answer_text\'+i+\'" type="text" class="validate"/>\n' +
+            '            <label for="answer_text\'+i+\'">Answer</label>\n' +
+            '        </div>\n' +
+            '    </div>')
+    }
 });
