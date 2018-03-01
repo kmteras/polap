79d40978d8756a4fa9adf5f591992994b7c73442
Johannes Jyrjo <johannes.jyrjo@gmail.com>
2018-03-01 15:46:57 +0200

    fixed database init

--- src/main/java/com/eucolus/poll/Entities/QuestionAnswer.java
+++ src/main/java/com/eucolus/poll/Entities/QuestionAnswer.java
@@ -9,4 +9,20 @@ public class QuestionAnswer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
